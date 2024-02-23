d2433aba1b022053b89c64de1ff4f137cdc52d3f
hiro1321 <>
2024-02-23 15:57:33 +0900

    add comment

--- reservations/api_reservation/views.py
+++ reservations/api_reservation/views.py
@@ -13,7 +13,19 @@ from ..serializers import RoomSerializer
 
 @api_view(["GET"])
 def getRoomAvailability(request, date):
-    """対象日付の予約可能な部屋数を返す"""
+    """
+    指定された日付における部屋の空き状況を取得するAPIエンドポイント。
+
+    Parameters:
+        request (HttpRequest): HTTPリクエストオブジェクト。
+        date (str): 日付を表す文字列（YYYYMMDD形式）。
+
+    Returns:
+        JsonResponse: 空き部屋の数を含むJSON形式のレスポンス。
+
+    Raises:
+        ValueError: date引数が無効な日付形式の場合に発生。
+    """
     # 予約可能な部屋の数を取得
     room_cnt = Room.objects.filter(availability=True).count()
 
@@ -24,26 +36,4 @@ def getRoomAvailability(request, date):
     ).count()
 
     result = room_cnt - reservation_count
-    print(result)
     return JsonResponse({"result": result})
-
-
-def room_detail(request, room_id):
-    try:
-        room = Room.objects.get(pk=room_id)
-    except Room.DoesNotExist:
-        return JsonResponse({"error": "Room not found"}, status=404)
-
-    if request.method == "GET":
-        serializer = RoomSerializer(room)
-        return JsonResponse(serializer.data)
-
-    elif request.method == "PUT":
-        data = json.loads(request.body.decode("utf-8"))
-        serializer = RoomSerializer(room, data=data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        else:
-            return JsonResponse(serializer.errors, status=400)
