94caf0321718020f94d20a5818fda8e35d258a83
hiro1321 <>
2024-03-15 21:05:59 +0900

    update

--- reservations/management/commands/superuser.py
+++ reservations/management/commands/superuser.py
@@ -2,15 +2,15 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.conf import settings
 
-CustomUser = get_user_model()
+User = get_user_model()
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not CustomUser.objects.filter(username=settings.SUPERUSER_NAME).exists():
-            CustomUser.objects.create_superuser(
+        if not User.objects.filter(username=settings.SUPERUSER_NAME).exists():
+            User.objects.create_superuser(
                 username=settings.SUPERUSER_NAME,
                 email=settings.SUPERUSER_EMAIL,
                 password=settings.SUPERUSER_PASSWORD,
             )
-            print("スーパーユーザー作成に成功しました")
+            print("スーパーユーザー作成")
